67957b7b63b9ee8bea63c7adf422a56b32b5a68c
Tomas Porchetto <porchettotomas@gmail.com>
2023-07-13 22:19:04 -0300

    f

---
+++ word_guess.py
@@ -0,0 +1,53 @@
+"""
+File: word_guess.py
+-------------------
+Fill in this comment.
+"""
+
+
+import random
+
+
+LEXICON_FILE = "Lexicon.txt"    # File to read word list from
+INITIAL_GUESSES = 8             # Initial number of guesses player starts with
+
+
+def play_game(secret_word):
+    """
+    Add your code (remember to delete the "pass" below)
+    """
+    pass
+
+
+def get_word():
+    """
+    This function returns a secret word that the player is trying
+    to guess in the game.  This function initially has a very small
+    list of words that it can select from to make it easier for you
+    to write and debug the main game playing program.  In Part II of
+    writing this program, you will re-implement this function to
+    select a word from a much larger list by reading a list of words
+    from the file specified by the constant LEXICON_FILE.
+    """
+    index = random.randrange(3)
+    if index == 0:
+        return 'HAPPY'
+    elif index == 1:
+        return 'PYTHON'
+    else:
+        return 'COMPUTER'
+
+
+def main():
+    """
+    To play the game, we first select the secret word for the
+    player to guess and then play the game using that secret word.
+    """
+    secret_word = get_word()
+    play_game(secret_word)
+
+
+# This provided line is required at the end of a Python file
+# to call the main() function.
+if __name__ == "__main__":
+    main()
